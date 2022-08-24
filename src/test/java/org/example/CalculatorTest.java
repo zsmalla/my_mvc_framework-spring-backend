550e5936ceccabeee46309000f7cc68a9b31c823
lim <malla7454@gmail.com>
2022-08-24 18:49:12 +0900

    사칙연산 계산기 : 조금 더 객체지향적인 방법

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.assertj.core.util.Streams;
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,6 +11,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
@@ -25,31 +27,40 @@ public class CalculatorTest {
     @DisplayName("덧셈 연산을 수행한다.")
     @Test
     void additionTest() {
-        int result = Calculator.calculate(1, "+", 2);
+        int result = Calculator.calculate(new PositiveNumber(1), "+", new PositiveNumber(2));
         assertThat(result).isEqualTo(3);
     }
 
     @DisplayName("뺄셈 연산을 수행한다.")
     @Test
     void subtractionTest() {
-        int result = Calculator.calculate(1, "-", 2);
+        int result = Calculator.calculate(new PositiveNumber(1), "-", new PositiveNumber(2));
         assertThat(result).isEqualTo(-1);
     }
 
     @DisplayName("테스트 리팩토링 버전")
     @ParameterizedTest
     @MethodSource("formulaAndResult")
-    void calculateTest(int operand1, String operator, int operand2, int result){
+    void calculateTest(PositiveNumber operand1, String operator, PositiveNumber operand2, int result){
         int calculateResult = Calculator.calculate(operand1, operator, operand2);
         assertThat(calculateResult).isEqualTo(result);
     }
 
     private static Stream<Arguments> formulaAndResult(){
         return Stream.of(
-                arguments(1, "+", 2, 3),
-                arguments(1, "-", 2, -1),
-                arguments(4, "*", 2, 8),
-                arguments(4, "/", 2, 2)
+                arguments(new PositiveNumber(1), "+", new PositiveNumber(2), 3),
+                arguments(new PositiveNumber(1), "-", new PositiveNumber(2), -1),
+                arguments(new PositiveNumber(4), "*", new PositiveNumber(2), 8),
+                arguments(new PositiveNumber(4), "/", new PositiveNumber(2), 2)
         );
     }
+
+    // operand가 모두 양수임이 보장되므로 아래 테스트는 여기서 불필요, 별도의 테스트 파일을 만들어 ExceptionTest 하는 것을 권장
+//    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.")
+//    @Test
+//    void calculateExceptionTest() {
+//        assertThatCode(()-> Calculator.calculate(new PositiveNumber(10), "/", new PositiveNumber(0)))
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessage("0으로는 나눌 수 없습니다.");
+//    }
 }
