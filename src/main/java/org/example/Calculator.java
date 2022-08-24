5024f052c5c76034ed8cdadfd82639ce9b023b1d
lim <malla7454@gmail.com>
2022-08-24 17:47:22 +0900

    사칙연산 계산기 : enum을 통한 계산 로직 구현

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Calculator {
+
+    public static int calculate(int operand1, String operator, int operand2){
+        return ArithmeticOperator.calculate(operand1, operator, operand2);
+    }
+//    public static int calculate(int operand1, String operator, int operand2) {
+//        if ("+".equals(operator)) {
+//            return operand1 + operand2;
+//        } else if("-".equals(operator)){
+//            return operand1 - operand2;
+//        } else if("*".equals(operator)){
+//            return operand1 * operand2;
+//        } else if ("/".equals(operator)){
+//            return operand1 / operand2;
+//        }
+//
+//        return 0;
+}
