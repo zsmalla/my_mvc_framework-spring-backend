bb4ad9ca6208ceaf5b40a87b81dcf899c4525bf3
lim <malla7454@gmail.com>
2022-09-01 23:01:30 +0900

    학점 계산기 : TDD방식 구현

---
+++ src/main/java/org/example/GradeCalculator.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.List;
+
+public class GradeCalculator {
+    private final List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+    /**
+     * 요구사항
+     * 평균학점 계산 방법 = (학점수*교과목 평점)의 합계/수강신청 총 학점 수
+     * 일급 컬렉션 사용
+     */
+    public double calculateGrade() {
+        double multipliedCreditAndCourseGrade = 0;
+        // (학점수 * 교과목 평점)의 합계
+        for (Course course : courses){
+            multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+        }
+
+        // 수강신청 총 학점 수
+        int totalCompletedCredit = courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+
+        return multipliedCreditAndCourseGrade / totalCompletedCredit;
+    }
+}
